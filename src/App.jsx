68683402bbff5590c4971b42eaf5f78e108bdd36
Karla Vujevic <karla_vu@icloud.com>
2023-09-10 17:54:17 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import ConditionalRendering from './ConditionalRendering.jsx'
+
+function App() {
+  return (
+    <>
+      <ConditionalRendering number={3}/>
+    </>
+  )
+}
+
+export default App
